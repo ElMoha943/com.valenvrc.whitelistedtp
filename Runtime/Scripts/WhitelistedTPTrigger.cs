59179d9f9d3ebce91b88f2ee020fc3011aff14bd
Valentino Casadidio <valentinodidio943@gmail.com>
2026-01-17 18:05:30 -0300

    Added whitelistedtptrigger script andp refab

---
+++ Runtime/Scripts/WhitelistedTPTrigger.cs
@@ -0,0 +1,49 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace valenvrc.WhitelistedTP
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    [DisallowMultipleComponent, Icon("Packages/com.valenvrc.whitelistedtp/Editor/Resources/whitelistedtpicon.png"), HelpURL("https://docs.valenvrc.com/free-assets/whitelisted-tp")]
+    public class WhitelistedTPTrigger : UdonSharpBehaviour
+    {
+        [SerializeField, Tooltip("Should the teleport use a whitelist?")]
+        bool useWhitelist = true;
+        [SerializeField, Tooltip("List of names that can use the teleport")]
+        string[] whitelist;
+        [SerializeField, Tooltip("Where to teleport the player to")]
+        Transform destination;
+        [SerializeField, Tooltip("Should the master of the instance always be able to teleport?")]
+        bool MasterByPass = true;
+        [SerializeField, Tooltip("Should the owner of the instance always be able to teleport?")]
+        bool OwnerByPass = true;
+
+        VRCPlayerApi localPlayer;
+        
+        void Start()
+        {
+            localPlayer = Networking.LocalPlayer;
+        }
+
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if(!player.isLocal) return;
+            if (!useWhitelist || (MasterByPass && Networking.IsMaster) || (OwnerByPass && Networking.IsOwner(gameObject)))
+            {
+                localPlayer.TeleportTo(destination.position, destination.rotation);
+                return;
+            }
+            foreach (string name in whitelist)
+            {
+                if (name == localPlayer.displayName)
+                {
+                    localPlayer.TeleportTo(destination.position, destination.rotation);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
